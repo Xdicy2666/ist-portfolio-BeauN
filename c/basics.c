c38aed5e1a38afc050f8fcb69d2bd2abdc5300de
Xdicy2666 <beaunewman8@gmail.com>
2024-01-22 14:47:53 -0500

    message

---
+++ c/basics.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("this is my c basics file\n")
+	
+		int num;
+		printf("Enter a number: ");
+			scanf("%i", &num);	
+			printf("Your number was %i\n", num);
+		return 0;
+}
